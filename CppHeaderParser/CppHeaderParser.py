52e3bb0a06737a4d026b374a84909a303c33135c
Tom Rix <trix@redhat.com>
2024-01-11 08:16:09 -0500

    cppheaderparser silence invalid escape sequence
    Signed-off-by: Tom Rix <trix@redhat.com>

--- CppHeaderParser/CppHeaderParser.py
+++ CppHeaderParser/CppHeaderParser.py
@@ -149,7 +149,7 @@ def t_COMMENT_MULTILINE(t):
         #not sure why, but get double new lines
         v = t.value.replace("\n\n", "\n")
         #strip prefixing whitespace
-        v = re.sub("\n[\s]+\*", "\n*", v)
+        v = re.sub(r"\n[\s]+\*", "\n*", v)
         doxygenCommentCache += v
     t.lexer.lineno += len([a for a in t.value if a=="\n"])
 def t_NEWLINE(t):
@@ -834,7 +834,7 @@ class CppMethod( _CppMethod ):
             doxyLines = self["doxygen"].split("\n")
             lastParamDesc = ""
             for doxyLine in doxyLines:
-                if " @param " in doxyLine or " \param " in doxyLine:
+                if " @param " in doxyLine or r" \param " in doxyLine:
                     try:
                         #Strip out the param
                         doxyLine = doxyLine[doxyLine.find("param ") + 6:]
